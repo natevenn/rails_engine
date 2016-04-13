f16d7002e84589cc2d633de3da6b8491064dbc52
Nathan Venn <natevenn@gmail.com>
2016-04-12 22:14:31 -0600

    fix issue in customer transactions controller

--- app/controllers/api/v1/customers/transactions_controller.rb
+++ app/controllers/api/v1/customers/transactions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Customers::TransactionsController < ApplicationController
   respond_to :json
 
   def index
-    invoices = Invoice.where(customer_id: 1)
+    invoices = Invoice.where(customer_id: params['customer_id'])
     respond_with Transaction.where(invoice_id: invoices)
   end
 end
